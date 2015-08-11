525508e4dcc12875e22b3f8e0a8941ed4d83f89b
Yafei Xu <xuyafee@gmail.com>
2015-08-11 15:59:57 +0200

    new change

--- 809histogram_simple/MMSTAThistogram_simple.r
+++ 809histogram_simple/MMSTAThistogram_simple.r
@@ -1,13 +1,13 @@
 # ------------------------------------------------------------------------------
-# Book:         Test Book
+# Book:         Introduction to Statistics using interactive MM*Stat elements
 # ------------------------------------------------------------------------------
 # Quantlet:     MMSTAThistogram_simple
 # ------------------------------------------------------------------------------
 # Description:  It produces an interactive interface to show the histogram. In 
 #               the most basic version only a histogram of the TOTALAREA from
 #               the USCRIME data set is shown. The user can interactively choose
-#               the number of bins. The user can choose the variables and switch
-#               to the other data set including CARS and DECATHLON.
+#               the number of bins. The user can choose variables and other data
+#               sets such as CARS and DECATHLON.
 # ------------------------------------------------------------------------------
 # Datafiles:    CARS.rds, DECATHLON.rds, USCRIME.rds
 # ------------------------------------------------------------------------------
@@ -31,6 +31,7 @@
 # ------------------------------------------------------------------------------
 # Author:       Yafei Xu
 # ------------------------------------------------------------------------------ 
+
 
 
 
